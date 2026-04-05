d5a2a704f6fe506ec04952b8c2d3de5c38dfa1a3
Bahram-Br <brr113114@gmail.com>
2026-04-05 13:18:47 -0400

    This extra test case for the Git

---
+++ src/day01/TestArrayInToGit.java
@@ -0,0 +1,20 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestArrayInToGit {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(list);
+
+        for(int i = 0; i < list.size(); i++){
+            if (list.get(i) % 2 == 0){
+                list.set(i, list.get(i) + 10);
+            }
+        }
+        System.out.println(list);
+    }
+}
